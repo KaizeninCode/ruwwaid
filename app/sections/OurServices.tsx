07cc6195cb75b615330f89afa9baf83f19909977
KaizeninCode <philwekullo@gmail.com>
2025-09-18 16:48:34 +0300

    feat: fix images on services section

--- app/sections/OurServices.tsx
+++ app/sections/OurServices.tsx
@@ -57,11 +57,11 @@ const OurServices = () => {
         '/vivacom.png'
     ]
     return (
-        <section id='our-services' className='max-w-7xl mx-auto px-4 py-16 min-h-screen'>
+        <section id='our-services' className='mx-auto px-4 lg:py-16 min-h-screen'>
           <h1 className="lg:text-5xl text-3xl font-bold mb-4 text-[#a47138] text-center">Our Services</h1> 
 
             <p className='mb-8 text-center lg:text-2xl'>We bring deep functional expertise in the following domains:</p>
-          <div className="grid lg:grid-cols-2 gap-16 lg:my-24">
+          <div className="lg:grid lg:grid-cols-2 max-lg:flex max-lg:flex-col-reverse gap-16 lg:my-24">
                 <div>
                     <h1 className="lg:text-4xl text-xl text-[#a47138] mb-4">Human Resource Consulting</h1>
                     {hrServices.map((service) => (
@@ -73,10 +73,14 @@ const OurServices = () => {
                         <li key={strategy} className='lg:text-xl ml-4'>{strategy}</li>
                     ))}
                 </div>
-            <div className='bg-[url(/services1.jpg) max-md:hidden bg-cover bg-center rounded-xl shadow-xl max-lg:h-[500]'/>
+            <div>
+                    <img src="/services3.jpg" alt="Services" className='rounded-lg shadow-2xl'/>
+                </div >
           </div>
           <div className="grid lg:grid-cols-2 gap-16 lg:my-24 my-8">
-                <div className='max-md:hidden bg-[url(/services2.jpg) bg-cover bg-center rounded-lg shadow-lg h-[500]'/>
+                <div>
+                    <img src="/services2.jpg" alt="Services" className='rounded-lg shadow-2xl '/>
+                </div >
                 <div>
                     <h1 className="lg:text-4xl text-xl text-[#a47138] mb-4">Labor Law and Compliance Advisory</h1>
                     {labourLaws.map((service) => (
@@ -89,7 +93,7 @@ const OurServices = () => {
                     ))}
                 </div>
           </div>
-          <div className="grid lg:grid-cols-2 gap-16 lg:my-24 my-8">
+          <div className="lg:grid lg:grid-cols-2 max-lg:flex max-lg:flex-col-reverse gap-16 lg:my-24 my-8">
                 <div>
                     <h1 className="lg:text-4xl text-xl text-[#a47138] mb-4">Industry Experience</h1>
                     {experiencePoints.map((service) => (
@@ -101,7 +105,10 @@ const OurServices = () => {
                         <li key={strategy} className='lg:text-xl ml-4'>{strategy}</li>
                     ))}
                 </div>
-                <div className='max-lg:hidden bg-[url(/services1.jpg) bg-cover bg-center rounded-lg shadow-lg h-[500]'/>
+                {/* <div className='max-lg:hidden bg-[url(/services1.jpg) bg-cover bg-center rounded-lg shadow-lg h-[500]'/> */}
+                <div>
+                    <img src="/services1.jpg" alt="Services" className='rounded-lg shadow-2xl'/>
+                </div >
           </div>
             <h1 className="lg:text-5xl text-xl text-[#a47138] font-semibold text-center lg:mb-16 mb-8">Who We Have Worked With</h1>
           <div className="grid grid-cols-3 gap-10">
